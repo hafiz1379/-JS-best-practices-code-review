c34d8ad404b7c143fd0fc1d5a90f17541582000f
hafiz1379 <hafizrasa1379@gmail.com>
2023-05-16 13:30:41 +0430

    Updated the script.js

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 
-function addTask() {
+function addNewTask() {
   const description = addInput.value;
   if (description) {
-    Task.addTask(description);
-    addInput.value = '';
-    renderTasks();
+  Task.addTask(description);
+  addInput.value = '';
+  renderTasks();
   }
-}
-
-renderTasks();
-
-addButton.addEventListener('click', () => {
-  addTask();
-});
-
-addInput.addEventListener('keypress', (event) => {
+  }
+  renderTasks();
+  addButton.addEventListener('click', () => {
+  addNewTask();
+  });
+  addInput.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
-    addTask();
+  addNewTask();
   }
-});
-
-clearAllButton.addEventListener('click', () => {
-  const tasks = Task.getTasks();
-  const newTasks = tasks.filter((task) => !task.completed);
-  newTasks.forEach((task, index) => {
-    task.id = index + 1;
+  });
+  clearAllButton.addEventListener('click', () => {
+  Task.clearAllTasks();
+  renderTasks();
   });
 
-  Task.setTasks(newTasks);
-  renderTasks();
-});
+  clearAllButton.addEventListener('click', () => {
+    const tasks = Task.getTasks();
+    const newTasks = tasks.filter((task) => !task.completed);
+    newTasks.forEach((task, index) => {
+    task.id = index + 1;
+    });
+    // Remove the event listener to avoid potential memory leaks
+    clearAllButton.removeEventListener('click', clearAllButtonClickHandler);
+    renderTasks();
+    });
+
+ 
